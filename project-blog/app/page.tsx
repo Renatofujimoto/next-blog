5ec998a4dbc6effb8e8431a1f0e23b8cc3cb9cb7
Renatofujimoto <renatofujimoto2@gmail.com>
2023-09-07 20:59:20 -0300

    feat: finish project

--- project-blog/app/page.tsx
+++ project-blog/app/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 async function fetchBlogs() {
   const res = await fetch("http://localhost:3000/api/blog", {
     next: {
-      revalidate:10,
-    }
+      revalidate: 10,
+    },
   });
   const data = await res.json();
   return data.posts;
@@ -12,35 +12,53 @@ async function fetchBlogs() {
 
 export default async function Home() {
   const posts = await fetchBlogs();
-  console.log(posts);
-
   return (
-    <>
-      <main className="w-full h-full">
-        <div className="md:w-2/4 sm:w-3/4 m-auto p-4 my-4 rounded-lg bg-slate-800 drop-shadow-xl">
-          <h1 className="text-slate-200 text-center text-2xl font-extrabold font-[verdana]">
-            Tech Blog
-          </h1>
-        </div>
+    <main className="w-full h-full">
+      <div className="md:w-2/4 sm:w-3/4 m-auto p-4 my-5 rounded-lg bg-slate-800 drop-shadow-xl">
+        <h1 className="text-slate-200 text-center text-2xl font-extrabold font-[verdana]">
+          Welcome, this is my project fullstack 
+        </h1>
+      </div>
+     
+      {/* Blogs */}
+      <div className="w-full flex  flex-col justify-center items-center">
+        {posts?.map((post: any) => (
+          <div className="w-3/4 p-4 rounded-md mx-3 my-2 bg-slate-200 flex flex-col justify-center">
 
-        <div className="flex my-5">
-          <Link href="/blog/add" className="md:w-1/6 sm:w-2/4 rounded-md text-center p-2 m-auto bg-slate-200 font-semibold">Add Post</Link>
-        </div>
-
-        {/* Blogs */}
-        <div className="w-full flex flex-col justify-center items-center">
-          {posts?.map((post:any) => (
-            <div className="w-3/4 p-4 rounded-md mx-3 my-2 bg-slate-200 flex flex-col justify-center">
-              <div className="flex items-center-my-3">
-                <div className="mr-auto">
-                  <h2 className="mr-auto font-semilbold">{post.title}</h2>
-                </div>
+            {/* Title and Action */}
+            <div className="flex items-center my-3">
+              <div className="mr-auto">
+                <h2 className="mr-auto font-semibold">{post.title}</h2>
               </div>
+              <Link
+                href={`/blog/edit/${post.id}`}
+                className="px-4 py-1  text-center text-xl bg-slate-900 rounded-md font-semibold text-slate-200"
+              >
+                Edit
+              </Link>
             </div>
-            
-          ))}
+            <div className=" mr-auto my-1 font-[roboto]">
+              <h2>{post.description}</h2>
+            </div>
+
+            {/* Date & Description */}
+            <div className="mr-auto my-1">
+              <blockquote className="font-bold text-slate-700">
+                {new Date(post.date).toDateString()}
+              </blockquote>
+            </div>
+          </div>
+        ))}
         </div>
-      </main>
-    </>
-  )
+        {/* Link */}
+        <div className="flex my-5">
+          <Link
+            href={"/blog/add"}
+            className=" md:w-1/6 sm:w-2/4 text-center rounded-md p-2 m-auto bg-slate-200 font-semibold"
+          >
+            Add New Post 🚀
+          </Link>
+        </div>
+    </main>
+  );
 }
