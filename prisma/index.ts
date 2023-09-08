3a2a6a48ef9b20cbf9a1720751812f9893cc8bca
Renatofujimoto <renatofujimoto2@gmail.com>
2023-09-07 21:16:55 -0300

    fix: prisma build

--- prisma/index.ts
+++ prisma/index.ts
@@ -9,13 +9,6 @@ declare global {
   }
 }
 
-if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
 
 export default prisma;
